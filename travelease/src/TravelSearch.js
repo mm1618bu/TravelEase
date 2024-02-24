0496659af5d8fe8044c305127a6483970f6edd5d
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2024-02-24 22:02:16 +0000

    Modifications to Search Page

--- travelease/src/TravelSearch.js
+++ travelease/src/TravelSearch.js
@@ -18,6 +18,29 @@ const TravelSearch = () => {
           <option value="plane">Plane</option>
           <option value="bus">Bus</option>
         </select>
+        <label for="start">Start Date:</label>
+        <input type="date" id="start" name="trip-start" value="2021-07-22" min="2021-01-01" max="2021-12-31"/>
+        <label for="end">End Date:</label>
+        <input type="date" id="end" name="trip-end" value="2021-07-22" min="2021-01-01" max="2021-12-31"/>
+        <label for="adults">Number of Adults:</label>
+        <input type="number" id="adults" name="adults" min="0"/>
+        <label for="children">Number of Children:</label>
+        <input type="number" id="children" name="children" min="0"/>
+        <label for="infants">Number of Infants:</label>
+        <input type="number" id="infants" name="infants" min="0"/>
+        <label>Cabin Class:</label>
+        <select id="cabin-class" name="cabin-class">
+          <option value="economy">Economy</option>
+          <option value="premiumEconomy">Premium Economy</option>
+          <option value="business">Business</option>
+          <option value="first">First</option>
+        </select>
+        <label>Hotel Room Type:</label>
+        <select id="room-type" name="room-type">
+          <option value="single">Single</option>
+          <option value="double">Double</option>
+          <option value="suite">Suite</option>
+        </select>
         <button id="Login" type="submit">Search</button>
       </form>
     </div>
