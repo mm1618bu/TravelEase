63b6bc52697b2979fc72f180ff0a0f88f7321a3b
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2024-02-27 01:10:34 +0000

    Library Modifications

--- travelease/src/HomePage.js
+++ travelease/src/HomePage.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import './App.css'; // Import the CSS file we will create next
+import './App.css';
+import { FaPlane, FaBus, FaTrain, FaHotel, FaCar } from 'react-icons/fa';
 
 const HomePage = () => {
   return (
     <div className="home-container">
       <img src="logo.png" alt="Logo" className="logo" />
       <div className="circle-logos">
-        <img src="circle-logo1.png" alt="Circle Logo 1" className="circle-logo" />
-        <img src="circle-logo2.png" alt="Circle Logo 2" className="circle-logo" />
-        <img src="circle-logo3.png" alt="Circle Logo 3" className="circle-logo" />
-      </div>
+  <div className="circle-logo">
+    <FaPlane />
+    <p>Plane</p>
+  </div>
+  <div className="circle-logo">
+    <FaBus />
+    <p>Bus</p>
+  </div>
+  <div className="circle-logo">
+    <FaTrain />
+    <p>Train</p>
+  </div>
+  <div className="circle-logo">
+    <FaHotel />
+    <p>Hotel</p>
+  </div>
+  <div className="circle-logo">
+    <FaCar />
+    <p>Car</p>
+  </div>
+</div>
       <p>Popular Destinations</p>
       <input type="text" placeholder="Search..." className="search-box" />
       <img src="wide-image.png" alt="Wide Image" className="wide-image" />
--- travelease/src/Login.js
+++ travelease/src/Login.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import './App.css'; // Import the CSS file we will create next
+import travel1 from './images/travel1.png';
+import './App.css';
+import { FaFacebook } from 'react-icons/fa';
+import { AiFillGoogleCircle, AiFillLinkedin, AiOutlineTwitter } from 'react-icons/ai';
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <h3>Sign in to Continue</h3>
-      <label>Email</label>
-      <input type="email" />
-      <label>Password</label>
-      <input type="password" />
-      <button id="Login">Log In</button>
-      <div className="social-media-login">
-        <button>SCM</button>
-        <button>SCM</button>
-        <button>SCM</button>
-        <button>SCM</button>
+    <div className="login-major">
+      <div className="login-container">
+        <h1>Login</h1>
+        <h3>Sign in to Continue</h3>
+        <label>Email</label>
+        <input type="email" required/>
+        <label>Password</label>
+        <input type="password" required/>
+        <button id="Login">Log In</button>
+        <div className="social-media-login">
+          <FaFacebook size={40} />
+          <AiFillGoogleCircle size={40} />
+          <AiFillLinkedin size={40} />
+          <AiOutlineTwitter size={40} />
+        </div>
+      </div>
+      <div className='imagelogin'>
+        <img src={travel1} alt="Travel" />
       </div>
     </div>
   );
